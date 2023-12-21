d80a08133e88fbbcb14d74ab575a045acf883a11
zatomis <zatomis@list.ru>
2023-12-21 14:59:19 +0300

    PEP8

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from textwrap import dedent
-
 import requests
 import telegram as telegram
 from environs import Env
 import time
 import os
+
 
 def get_devman_statistic(api_key, params):
     url = "https://dvmn.org/api/long_polling/"
@@ -12,6 +11,7 @@ def get_devman_statistic(api_key, params):
     response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
     return response.json()
+
 
 def check_work(student_token, params, bot, tg_group_id):
     while True:
@@ -33,18 +33,9 @@ def check_work(student_token, params, bot, tg_group_id):
                     if current_review['is_negative']:
                         bot.send_message(chat_id=tg_group_id,
                                          text=f'Pаботa ⛔️ *{current_review["lesson_title"]}*\nК сожалению, в работе нашлись ошибки\n[Ссылка на работу]({current_review["lesson_url"]})')
-                                         # text=f'{current_review["lesson_title"]}')
                     else:
                         bot.send_message(chat_id=tg_group_id,
                                          text=f'Pаботa ✅ *{current_review["lesson_title"]}*\n Принята.\n[Ссылка на работу]({current_review["lesson_url"]})')
-
-                        # text=dedent(f"""
-                        #                  Преподаватель проверил работу *"{current_review["lesson_title"]}"*\
-                        #                  Преподавателю все понравилось, можно приступать к следующему уроку\
-                        #                  """),
-                        #                  parse_mode=telegram.ParseMode.MARKDOWN_V2,
-                        #                  )
-
 
 
 if __name__ == '__main__':
@@ -54,6 +45,6 @@ if __name__ == '__main__':
     token_student = env('STUDENT_TOKEN')
     telegram_bot = os.environ.get("TELEGRAMBOT_KEY", "ERROR")
     telegram_chanel = os.environ.get("TELEGRAMBOTGROUP", "ERROR")
-    params = {'timestamp': 0,}
+    params = {'timestamp': 0, }
     bot = telegram.Bot(token=telegram_bot)
     check_work(token_student, params, bot, telegram_chanel)
